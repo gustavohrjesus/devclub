7bc6c22e0686bb2bc3d19f39ca674914c64b225b
Gustavo H Rodrigues de Jesus <rodrigues.gustavoh@gmail.com>
2024-10-11 13:06:09 -0300

    :bug: fix(back): solve password comparation to acc
    Foi solucionado o problema no "Login", onde podia-se passar um usuario
    valido e uma senha invalida, que o acesso era aceito.
    O exito foi obtido somente quando a funcao bcrypt.compare era usada no
    modo de ter um callback de resposta. Da forma que estavamos fazendo a
    comparacao e passando o resultado para a constante "isMatch", obtinhamos
    erro ao usar a palavra reservada "await".

--- login_backend__api_node_express_jwt/routes/public.js
+++ login_backend__api_node_express_jwt/routes/public.js
@@ -10,68 +10,75 @@ const JWT_SECRET = process.env.SECRET
 
 // ------------- LOGIN - BEGIN ------------
 router.post('/login', async (req, res) => {
-    const userInfo = req.body
+    try{
+        console.log(`ENTROU TRY`)
+        const userInfo = req.body
 
-    await db.query( 'SELECT * FROM usuarios WHERE email = ? AND ativo = "s"', [userInfo.email], (err, response) => {
-        if(err){
-            return res.status(500).json( { message: "Erro no servidor. Tente mais tarde!" } )
-            console.log(err.sqlMessage)
-        } 
-        
-        if(response.length > 0){ // Se maior que zero, temos um usuario (email) e senha validos no bd
-            const isMatch = bcrypt.compare(userInfo.password, response[0].password)
-            if(!isMatch){
-                return res.status(400).json( { message: "Senha invalida!" } )
+        await db.query( 'SELECT * FROM usuarios WHERE email = ? AND ativo = "s"', [ userInfo.email ], (err, response) => {
+            if(err){
+                return res.status(500).json( { message: "Erro no servidor. Tente mais tarde!" } )
+                console.log(err.sqlMessage)
             }
-                
-            const token = jwt.sign( 
-                { id: response[0].idusuarios, perfil: response[0].nivelacesso }, 
-                JWT_SECRET, 
-                { expiresIn: '2m' } 
-            )
 
-            res.status(200).json( token )
-            // return res.status(200).json( response[0] )
-            console.log(response)
-        } else {
-            return res.status(404).json( { message: "Usuario nao localizado!" } )
-            console.log(response)
-        }
-    } )
+            if(response.length <= 0){ // Se maior que zero, temos um usuario (email) e senha validos no bd
+                return res.status(404).json({ msg: "Usuario nao localizado!" })
+            }
+
+            // const isMatch = await bcrypt.compare( userInfo.password, response[0].password ) // da erro com a palavra reservada 'await
+            bcrypt.compare( userInfo.password, response[0].password, ( erro, resu ) => {
+                if(!resu){
+                    return res.status(400).json({ msg: 'Senha Invalida!' })
+                }
+
+                const token = jwt.sign( 
+                    { id: response[0].idusuarios, perfil: response[0].nivelacesso }, 
+                    JWT_SECRET, 
+                    { expiresIn: '2m' } 
+                )
+    
+                res.status(200).json( token )
+                // return res.status(200).json( response[0] )
+                console.log(response)
+            } )
+        })
+
+    } catch(error){
+        console.log(`CATCH ERROR: ${ error }`)
+    }
 
 })
 // ------------- LOGIN - END --------------
 
 
 //! ------------- CADASTRO - BEGIN ------------
-// router.post( '/cadastro', async (req, res) => {    
-//     const user = req.body
+router.post( '/cadastro', async (req, res) => {    
+    const user = req.body
 
-//     const salt = await bcrypt.genSalt(10)
-//     const hashPassword = await bcrypt.hash(user.password, salt)
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(user.password, salt)
 
-//     await db.query( 'INSERT INTO usuarios ( email, password, nivelacesso, ativo) VALUES (?, ?, ?, ?)', 
-//         [ user.email, hashPassword, user.nivelacesso, user.ativo ], (err, response) => {
-//             if(err){
-//                 res.status(500).json( { message: "Erro no servidor! Tente novamente mais tarde." } )
-//                 // res.status(404).json(err)
-//                 console.log(err.sqlMessage)
+    await db.query( 'INSERT INTO usuarios ( email, password, nivelacesso, ativo) VALUES (?, ?, ?, ?)', 
+        [ user.email, hashPassword, user.nivelacesso, user.ativo ], (err, response) => {
+            if(err){
+                res.status(500).json( { message: "Erro no servidor! Tente novamente mais tarde." } )
+                // res.status(404).json(err)
+                console.log(err.sqlMessage)
                     
-//                 // return res.json({
-//                 //     erro: true,
-//                 //     msg: 'Aconteceu um erro no servidor. Tente novamente mais tarde: ' + err                   
-//                 //  })
-//             } 
-//             else {
-//                 res.status(201).json(`Usuario ${user.email} cadastrado com sucesso!`)
-//                 console.log(response)
-//                 // return res.json({
-//                 //     erro: false,
-//                 //     msg: `Rack cadastrada com sucesso!!!`
-//                 // })
-//             }
-//     } )
-// } )
+                // return res.json({
+                //     erro: true,
+                //     msg: 'Aconteceu um erro no servidor. Tente novamente mais tarde: ' + err                   
+                //  })
+            } 
+            else {
+                res.status(201).json(`Usuario ${user.email} cadastrado com sucesso!`)
+                console.log(response)
+                // return res.json({
+                //     erro: false,
+                //     msg: `Rack cadastrada com sucesso!!!`
+                // })
+            }
+    } )
+} )
 //! ------------- CADASTRO - END --------------
 
 //? ------------- CONSULTA - BEGIN ------------
